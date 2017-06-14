f63645da0acdf4f8c86c0ebb740ff00c7d0bbaa8
Manuel Ernst <seriousstuff@gmx.de>
2017-06-14 08:11:24 +0200

    animation cycle for ff

--- src/scenarios/floodfill.js
+++ src/scenarios/floodfill.js
@@ -3,30 +3,54 @@ import ff from './lib/floodfill'
 import generateBlankSquare from '../lib/generateBlankSquare'
 
 export default function floodfill(launchpad) {
-    launchpad.on('input', (x, y) => start(x, y))
+    let cycles = []
 
-    function start(x, y) {
-        let gens = ff({x: x, y: y})
-        let color = Color.getRandomColor()
-
-        function print() {
-            if (gens.length === 0) {
-                launchpad.updateBoard(generateBlankSquare(Color.BLACK))
-                return
+    // launchpad.on('input', add)
+    
+    let position = {x: 0, y: 0}
+    setInterval(() => {
+        while (true) {
+            let rndX = Math.random()
+            let rndY = Math.random()
+            let newPosition = {
+                x: rndX < 0.4 ? position.x - 1 : rndX < 0.8 ? position.x + 1 : position.x,
+                y: rndY < 0.4 ? position.y - 1 : rndY < 0.8 ? position.y + 1 : position.y
             }
-
-            let gen = gens.shift()
-            let blank = generateBlankSquare(Color.BLACK)
-
-            gen.forEach(entry => {
-                blank[entry.x][entry.y] = color
-            })
-
-            launchpad.updateBoard(blank)
-
-            setTimeout(print, 100)
+            
+            if (newPosition.x >= 0 && newPosition.x <= 7 && newPosition.y >= 0 && newPosition.y <= 7) {
+                position = newPosition
+                break
+            }
         }
 
-        print()
+        add(position.x, position.y)
+    }, 100)
+
+    function add(x, y) {
+        let gens = ff({x: x, y: y})
+        gens.color = Color.getRandomColor()
+
+        cycles.push(gens)
     }
+
+    function animationCycle() {
+        setTimeout(animationCycle, 100)
+
+        if (cycles.length === 0) return
+
+        let blank = generateBlankSquare(Color.BLACK)
+        cycles.forEach(cycle => {
+            let gen = cycle.shift()
+
+            gen.forEach(entry => {
+                blank[entry.x][entry.y] = cycle.color
+            })
+        })
+
+        launchpad.updateBoard(blank)
+
+        cycles = cycles.filter(cycle => cycle.length > 0)
+    }
+
+    animationCycle()
 }
