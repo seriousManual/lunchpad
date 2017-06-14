8672384e0293b257a571678c080606684c39f05a
Manuel Ernst <seriousstuff@gmx.de>
2017-06-14 07:31:39 +0200

    webpack config tweak

--- webpack.config.js
+++ webpack.config.js
@@ -3,37 +3,47 @@
 const path = require('path')
 const webpack = require('webpack')
 
-module.exports = [
-    {
-        devtool: 'cheap-module-source-map',
-        entry: [
-            './src/app.js'
-        ],
-        output: {
-            path: path.join(__dirname, 'build'),
-            filename: 'app.js'
-        },
-        module: {
-            loaders: [
-                {
-                    test: /\.jsx?$/,
-                    loader: 'babel-loader',
-                    exclude: [
-                        path.resolve(__dirname, 'node_modules')
+const baseConfig = {
+    entry: [
+        './src/app.js'
+    ],
+    output: {
+        path: path.join(__dirname, 'build'),
+        filename: 'app.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader',
+                exclude: [
+                    path.resolve(__dirname, 'node_modules')
+                ],
+                query: {
+                    presets: [
+                        ['react']
                     ],
-                    query: {
-                        presets: [
-                            ['react'],
-                            // ['es2015', { 'modules': false }]
-                        ],
-                        cacheDirectory: true
-                    }
+                    cacheDirectory: true
                 }
-            ]
-        },
-
-        plugins: [
-            //new webpack.optimize.UglifyJsPlugin()
+            }
         ]
     }
-]
+}
+
+
+if (process.env.NODE_ENV === 'production') {
+    baseConfig.plugins = [
+        new webpack.optimize.UglifyJsPlugin(),
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        })
+    ]
+
+    baseConfig.module.loaders[0].query.presets.push(['es2015', {'modules': false}])
+} else {
+    baseConfig.devtool = 'cheap-module-source-map'
+}
+
+module.exports = baseConfig
