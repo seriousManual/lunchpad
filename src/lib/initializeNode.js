43d4611a12110bb6c0a78b98b75d79a902ba95e0
Tilman Adler <tilman-git@t-animal.de>
2018-11-17 16:18:51 +0100

    support more than one board per computer

--- src/lib/initializeNode.js
+++ src/lib/initializeNode.js
@@ -1,13 +1,13 @@
 const LaunchpadNode = require('./LaunchpadNode');
 
-function initializeNode() {
+function initializeNode(launchpadNumber = 1) {
     return new Promise(resolve=> {
-        let {input, output} = _getLaunchpadNode()
+        let {input, output} = _getLaunchpadNode(launchpadNumber)
         resolve(new LaunchpadNode(input, output))
     })
 }
 
-function _getLaunchpadNode() {
+function _getLaunchpadNode(launchpadNumber) {
     const midi = require('midi')
     const input = new midi.input()
     const output = new midi.output()
@@ -15,8 +15,16 @@ function _getLaunchpadNode() {
     let lpInput = null
     let lpOutput = null
 
+    let inputsFound = 0
+    let outputsFound = 0
+
     for (let i = 0; i < input.getPortCount(); i++) {
         if (input.getPortName(i).indexOf('Launchpad Mini') >= 0) {
+            inputsFound++
+
+            if(inputsFound < launchpadNumber)
+                continue;
+
             input.openPort(i)
             lpInput = input
             break
@@ -25,14 +33,23 @@ function _getLaunchpadNode() {
 
     for (let i = 0; i < output.getPortCount(); i++) {
         if (output.getPortName(i).indexOf('Launchpad Mini') >= 0) {
+            outputsFound++
+
+            if(outputsFound < launchpadNumber)
+                continue;
+
             output.openPort(i)
             lpOutput = output
             break
         }
     }
 
-    if (!lpInput || !lpOutput) {
+    if(inputsFound == 0 || outputsFound == 0){
         throw new Error('no launchpad found')
+    }
+
+    if (!lpInput || !lpOutput) {
+        throw new Error('no launchpad for that device number found')
     }
 
     return {
