53d7b5f3012b290b5ef6e9b7068ff2663cf10860
Manuel Ernst <manuel.ernst@methodpark.de>
2019-07-12 09:45:19 +0200

    chores

--- examples/do.js
+++ examples/do.js
@@ -2,4 +2,3 @@ const initialize = require('../src/indexNode').initialize
 const drawing = require('./lib/drawing')
 
 initialize(1).then(launchpad => drawing(launchpad))
-initialize(2).then(launchpad => drawing(launchpad))
