49bee50621fc8ecc7d5107b0d91080362fda3bb2
Manuel Ernst <manuel.ernst@methodpark.de>
2019-07-16 08:49:30 +0200

    catch init errors

--- examples/do.js
+++ examples/do.js
@@ -1,4 +1,8 @@
 const initialize = require('../src/indexNode').initialize
 const drawing = require('./lib/drawing')
 
-initialize(1).then(launchpad => drawing(launchpad))
+initialize(1)
+    .then(launchpad => drawing(launchpad))
+    .catch(error => {
+        throw error
+    })
